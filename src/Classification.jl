88420df2dbe2b568779523e83e12791fc1ad45e5
Carlos Vázquez Monzón <carkol2010@hotmail.es>
2025-07-28 20:21:33 +0200

    Update Classification module

--- src/Classification.jl
+++ src/Classification.jl
@@ -7,10 +7,12 @@ include("./lib/classif/test_class_config.jl")
 include("./lib/classif/test_class_load.jl")
 include("./lib/classif/test_class_class.jl")
 include("./lib/classif/test_class_Es.jl")
+include("./lib/classif/test_class_plots.jl")
 include("./lib/classif/mrinterp.jl")
 include("./lib/classif/clsplot.jl")
 
 export test_class_class, ping_average0, test_class_Es, mrinterp, clsplot, test_class_load,
-ping_average1, ping_distance1, ping_distance1a, ping_distance2, ping_distance2a, ping_distance3
+ping_average1, ping_distance1, ping_distance1a, ping_distance2, ping_distance2a, ping_distance3,
+plot_class_map, plot_mean_std, plot_median_range, plot_min_max
 
 end
